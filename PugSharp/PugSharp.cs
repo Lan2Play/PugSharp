9f94a1f07d9409092db14e8e4fa486763cbbd924
TheR00st3r <andyhahn91@gmail.com>
2023-11-01 22:14:12 +0100

    check user if null

--- PugSharp/PugSharp.cs
+++ PugSharp/PugSharp.cs
@@ -321,6 +321,11 @@ public class PugSharp : BasePlugin, IMatchCallback
             // Give players max money if no match is running
             Server.NextFrame(() =>
             {
+                if (userId == null)
+                {
+                    return;
+                }
+
                 var player = new Player(userId);
 
                 // TODO read mp_maxmoney cvar
