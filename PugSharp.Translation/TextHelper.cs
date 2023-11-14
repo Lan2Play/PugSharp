487f9e285ffcca532dcf01f6342bf042d006c82d
TheR00st3r <andyhahn91@gmail.com>
2023-11-14 23:01:52 +0100

    Fix Translation

--- PugSharp.Translation/TextHelper.cs
+++ PugSharp.Translation/TextHelper.cs
@@ -18,25 +18,37 @@ namespace PugSharp.Translation
             _ErrorColor = errorColor;
         }
 
-        [GeneratedRegex(@"(\*\*(?<highglight>.*?)\*\*)", RegexOptions.ExplicitCapture, _RegexTimeout)]
+        [GeneratedRegex(@"(\*\*(?<highlight>.*?)\*\*)", RegexOptions.ExplicitCapture, _RegexTimeout)]
         private static partial Regex HighlightRegex();
 
         [GeneratedRegex(@"(`(?<command>.*?)`)", RegexOptions.ExplicitCapture, _RegexTimeout)]
         private static partial Regex CommandRegex();
 
 
-        [GeneratedRegex(@"(`!!(?<error>.*?)!!)", RegexOptions.ExplicitCapture, _RegexTimeout)]
+        [GeneratedRegex(@"(!!(?<error>.*?)!!)", RegexOptions.ExplicitCapture, _RegexTimeout)]
         private static partial Regex ErrorRegex();
 
         public string GetText(string key, params object?[] arguments)
         {
-            var text = Resources.ResourceManager.GetString(key, CultureInfo.CurrentCulture);
+
+            var text = Resources.ResourceManager.GetString(key.Replace('_', '.'), CultureInfo.CurrentCulture);
             if (text == null)
             {
                 return $"?key?";
             }
 
-            text = string.Format(CultureInfo.CurrentCulture, text, arguments);
+            if (arguments.Length > 0)
+            {
+                try
+                {
+                    var stringArguments = arguments.Select(GetArgumentString).ToArray<object>();
+                    text = string.Format(CultureInfo.CurrentCulture, text, stringArguments);
+                }
+                catch (Exception)
+                {
+                    // Do nothing
+                }
+            }
 
             if (HighlightRegex().IsMatch(text)
                 || CommandRegex().IsMatch(text)
@@ -50,6 +62,18 @@ namespace PugSharp.Translation
             text = ErrorRegex().Replace(text, match => $"{_ErrorColor}{match.Groups["error"].Value}{_DefaultColor}");
 
             return text;
+        }
+
+        private string GetArgumentString(object? value)
+        {
+            return value switch
+            {
+                string s => s,
+                int i => i.ToString(CultureInfo.CurrentCulture),
+                float f => f.ToString(CultureInfo.CurrentCulture),
+                double d => d.ToString(CultureInfo.CurrentCulture),
+                _ => value?.ToString() ?? string.Empty,
+            };
         }
     }
 }
