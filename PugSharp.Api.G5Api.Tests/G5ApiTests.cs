2577f3d9d1d8a7cc0a6b0d80d89e632220e015de
TheR00st3r <andyhahn91@gmail.com>
2023-11-30 17:52:30 +0100

    Test

--- PugSharp.Api.G5Api.Tests/G5ApiTests.cs
+++ PugSharp.Api.G5Api.Tests/G5ApiTests.cs
@@ -25,6 +25,21 @@ public partial class G5ApiTests : ApiTestBase
         Assert.True(await g5ApiClient.SendEventAsync(new MapPickedEvent() { Team = "team2", MapName = "de_mirage", MatchId = "1", MapNumber = 1 }, CancellationToken.None));
         Assert.True(await g5ApiClient.SendEventAsync(new GoingLiveEvent() { MapNumber = 1, MatchId = "1" }, CancellationToken.None));
 
+        Assert.True(await g5ApiClient.SendEventAsync(new RoundMvpEvent() { MapNumber = 1, MatchId = "1", Player = new Player("76561198025644200", "3", 3, Side.T, isBot: false), Reason = 2, RoundNumber = 1 }, CancellationToken.None));
+        Assert.True(await g5ApiClient.SendEventAsync(new RoundMvpEvent() { MapNumber = 1, MatchId = "1", Player = new Player("76561198025644200", "3", 3, Side.T, isBot: false), Reason = 2, RoundNumber = 2 }, CancellationToken.None));
+        Assert.True(await g5ApiClient.SendEventAsync(new RoundMvpEvent() { MapNumber = 1, MatchId = "1", Player = new Player("76561198025644200", "3", 3, Side.T, isBot: false), Reason = 2, RoundNumber = 3 }, CancellationToken.None));
+
+        var statsTeam1 = new StatsTeam("1", "Kraddlers", 1, 0, 0, 0, new List<StatsPlayer> {
+            new() {},
+            new() {},
+        });
+        var statsTeam2 = new StatsTeam("2", "Knaddles", 1, 3, 2, 1, new List<StatsPlayer> {
+            new() {},
+            new() {},
+         });
+        Assert.True(await g5ApiClient.SendEventAsync(new MapResultEvent() { MapNumber = 1, MatchId = "1", Winner = new Winner(Side.T, 0), StatsTeam1 = statsTeam1, StatsTeam2 = statsTeam2 }, CancellationToken.None));
+        Assert.True(await g5ApiClient.SendEventAsync(new SeriesResultEvent() { MatchId = "1", Winner = new Winner(Side.T, 0), Team1SeriesScore = 0, Team2SeriesScore = 1, TimeUntilRestore = 2 }, CancellationToken.None));
+
         // Assert
     }
 }
